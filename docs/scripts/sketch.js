2c67e2f7dc175380dc5853988d903a8f0b77a3c7
Juan Diego <juandiegomariscal@gmail.com>
2023-06-13 22:24:05 +0200

    deploy

---
+++ docs/scripts/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  lienzo = createCanvas(400, 400);
+  lienzo.parent('sketch')
+  noStroke()
+  fill(0)
+}
+
+let i = 0
+function draw() {
+  background(220);
+  fill(0)
+  rect(i, 0, 100)
+  i++
+  if(i > width){
+    i = 0
+  }
+  
+  fill(0,0,255)
+  rect(frameCount, height-100, 50)
+  
+  fill(255, 0, 0)
+  rect(frameCount%width, height-50, 50)
+}
